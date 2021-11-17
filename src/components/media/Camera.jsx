7b3da0e62ee0c9fe74a6b1dca3fcac1b4b7590ed
David Andersson <lejonmanen@gmail.com>
2021-11-17 15:02:55 +0100

    Finished MediaDevices component

--- src/components/media/Camera.jsx
+++ src/components/media/Camera.jsx
@@ -3,8 +3,16 @@ import './Media.css'
 
 const Camera = () => {
 	const [canUseMd, setCanUseMd] = useState(false)
+	const [statusMessage, setStatusMessage] = useState('')
+	const [cameraIsOn, setCameraIsOn] = useState(false)
 	const videoRef = useRef(null)
-	const handleCameraOn = () => cameraOn(videoRef.current)
+	const handleCameraToggle = () => {
+		if( cameraIsOn ) {
+			cameraOff(videoRef.current, () => setCameraIsOn(false))
+		} else {
+			cameraOn(videoRef.current, setStatusMessage, () => setCameraIsOn(true))
+		}
+	}
 
 	useEffect(() => {
 		// Körs en gång, när komponenten blir monuted
@@ -15,25 +23,34 @@ const Camera = () => {
 		<div className="camera-container">
 		{canUseMd ? <video ref={videoRef}></video> : null}
 			<div>
-				<button onClick={handleCameraOn}> Camera on </button>
+				<button onClick={handleCameraToggle}>
+				{cameraIsOn ? 'Turn camera off' : 'Turn camera on'}
+				</button>
 			</div>
+			<p> {statusMessage} </p>
 		</div>
 	)
 }
 
-async function cameraOn(videoElement) {
-	if( !('mediaDevices' in navigator) ) {
-		// meddela användaren
-		return
-	}
+async function cameraOff(videoElement, whenDone) {
+	videoElement.srcObject = null
+	whenDone()
+}
+async function cameraOn(videoElement, showMessage, whenDone) {
 	const constraints = {
-		video: { facingMode: 'user', width: 400, height: 300 }
+		video: { facingMode: 'user', width: 300, height: 200 }
 	}
-	const stream = await navigator.mediaDevices.getUserMedia(constraints)
-	videoElement.srcObject = stream
-	videoElement.addEventListener('loadedmetadata', () => {
-		videoElement.play()
-	})
+	try {
+		const stream = await navigator.mediaDevices.getUserMedia(constraints)
+		videoElement.srcObject = stream
+		videoElement.addEventListener('loadedmetadata', () => {
+			videoElement.play()
+			whenDone()
+		})
+	} catch(error) {
+		console.log('Could not use camera: ', error.message);
+		showMessage('Sorry, could not use your camera. Did you give me permission? Check that that you are not already using your camera in another app.')
+	}
 }
 
 export default Camera
